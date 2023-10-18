f2579d6a38eb88a2df6a1059b2d88c1864737f4d
Gajanan-bhosale <gajananbhosale.official@gmail.com>
2023-10-18 10:08:42 +0530

    Updated Code

---
+++ src/test/java/com/example/makemytrip/MakemytripApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.makemytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemytripApplicationTests {
+
+	public static void main(String[] args){
+        SpringApplication.run(MakemytripApplicationTests.class, args);
+    }
+
+}
+ 
